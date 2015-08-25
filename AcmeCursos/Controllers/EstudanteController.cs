07c53fcc6543a95379da96511b7affd81d0094d7
Romildo Jozué Paiter <romildopaiter@gmail.com>
2015-08-24 23:59:08 -0300

    Envio do Projeto para o Github concluido o vídeo 2 da unidade de Introdução ao C#

---
+++ AcmeCursos/Controllers/EstudanteController.cs
@@ -0,0 +1,147 @@
+﻿using AcmeCursos.DAL;
+using AcmeCursos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcmeCursos.Controllers
+{
+    public class EstudanteController : Controller
+    {
+
+        private AppDBContext db = new AppDBContext();
+
+        // GET: Estudante
+        public ActionResult Index()
+        {
+
+            var estudantes = db.Estudantes.ToList();
+
+            return View(estudantes);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Estudante estudante)
+        {
+
+            if (ModelState.IsValid)
+            {
+                db.Estudantes.Add(estudante);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(estudante);
+
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Estudante estudante = db.Estudantes.Find(id);
+
+            if ( estudante == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(estudante);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Estudante estudante)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(estudante).State = System.Data.Entity.EntityState.Modified;
+
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(estudante);
+        }
+
+        [HttpGet]
+        public ActionResult Details(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Estudante estudante = db.Estudantes.Find(id);
+
+            if(estudante == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(estudante);
+        }
+
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Estudante estudante = db.Estudantes.Find(id);
+
+            if (estudante == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(estudante);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Estudante estudante = db.Estudantes.Find(id);
+
+            db.Estudantes.Remove(estudante);
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        /*
+        * Liberando conexões do banco de dados
+        */
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+    }
+}
---
+++ AcmeCursos/Models/Curso.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AcmeCursos.Models
+{
+    [Table("Curso")]
+    public class Curso
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(15, MinimumLength = 3)]
+        public String Nome { get; set; }
+
+        [StringLength(250, MinimumLength = 0)]
+        [Display(Name ="Descrição")]
+        public String Descricao { get; set; }
+
+        [Required]
+        [Display(Name ="Data limite para inscrição")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime DataLimiteInscricao { get; set; }
+    }
+}
